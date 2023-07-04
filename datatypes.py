0f6c6d0791999385a8de5306efcfe3b1a6096311
APPIREDDIGARI VANDANA <135859797+VANDANAREDDY73@users.noreply.github.com>
2023-07-04 15:30:52 +0530

    Add files via upload

---
+++ datatypes.py
@@ -0,0 +1,20 @@
+a=10
+print(type(a))
+b=3.5
+print(type(b))
+n=2+3j
+print(type(n))
+print(n.real)
+print(n.imag)
+m=3-4j
+print(m.real)
+print(m.imag)
+
+
+x=20
+y=30
+z=50
+print(x, y, z, sep='-')
+print(x,end='&')
+print(y)
+print(z)

31ae1124d0fcacce07711ed302d6038a30ba3437
APPIREDDIGARI VANDANA <135859797+VANDANAREDDY73@users.noreply.github.com>
2023-07-06 07:35:25 +0530

    Add files via upload
    BOOLEAN DATATYPE:
    Boolean has only two values-True & False
    in python True is considered as 1 & False is considered as 0.
    STRING DATATYPE:
    -String is declared using single quotes (' ') ,double quotes(" ") ,Triple quotes(''' ''')
    -Multiline strings are declared using triple quotes.
    -It is ordered datatype ,it supports indexing both positive & negative. Positive starts with 0 (L-R), Negative (R-L)
    -String is immutable datatype, it can't be modified.

---
+++ datatypes1.py
@@ -0,0 +1,22 @@
+#BOOLEAN DATATYPE
+
+print(True + True + True)
+print(True + False)
+print(True +5)
+print(True)
+
+#STRING DATATYPE
+
+A='VANDANA'
+B='MOUNIKA'
+print(type(A))
+print(type(B))
+print(A[0])
+print(B[-2])
+print(A[7])
+
+
+
+
+
+

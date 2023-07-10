f1cfb2482568450d1f11d6d5ba0652ada7ca8880
APPIREDDIGARI VANDANA <135859797+VANDANAREDDY73@users.noreply.github.com>
2023-07-10 22:38:10 +0530

    Add files via upload

---
+++ operator_s.py
@@ -0,0 +1,18 @@
+Python 3.11.4 (tags/v3.11.4:d2340ef, Jun  7 2023, 05:45:37) [MSC v.1934 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+= RESTART: C:/Users/VANDANA/AppData/Local/Programs/Python/Python311/operators.py
+ABCDEF
+ABCABC
+DEFDEFDEF
+[10, 20, 30, 40, 50, 10]
+[10, 20, 30, 10, 20, 30]
+[40, 50, 10, 40, 50, 10, 40, 50, 10]
+(10, 20, 30, 10, 40, 50)
+(10, 20, 30, 10, 20, 30)
+(10, 40, 50, 10, 40, 50, 10, 40, 50)
+{20, 30}
+Traceback (most recent call last):
+  File "C:/Users/VANDANA/AppData/Local/Programs/Python/Python311/operators.py", line 25, in <module>
+    print(D1 * D2)
+TypeError: unsupported operand type(s) for *: 'dict' and 'dict'
---
+++ operators.py
@@ -0,0 +1,25 @@
+N1 = 'ABC'
+N2 = 'DEF'
+print(N1+N2)
+print(N1*2)
+print(N2*3)
+
+L1 = [10,20,30]
+L2 = [40,50,10]
+print(L1+L2)
+print(L1*2)
+print(3*L2)
+
+T1 = (10,20,30)
+T2 = (10,40,50)
+print(T1 + T2)
+print(T1 * 2)
+print(3 * T2)
+
+S1 = {10,20,30}
+S2 = {10,40,50}
+print(S1 - S2)
+
+D1={'X':10,'Y':30}
+D2={'P':40,'Q':30}
+print(D1 * D2)

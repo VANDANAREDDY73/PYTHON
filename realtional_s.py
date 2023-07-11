97659beb9884e58d771532330e369fcd88fa5c48
APPIREDDIGARI VANDANA <135859797+VANDANAREDDY73@users.noreply.github.com>
2023-07-11 23:15:24 +0530

    Add files via upload

---
+++ realtional_s.py
@@ -0,0 +1,457 @@
+Python 3.11.4 (tags/v3.11.4:d2340ef, Jun  7 2023, 05:45:37) [MSC v.1934 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+= RESTART: C:/Users/VANDANA/AppData/Local/Programs/Python/Python311/relational.py
+False
+True
+False
+True
+False
+True
+True
+False
+True
+False
+True
+False
+True
+False
+True
+False
+True
+False
+True
+
+= RESTART: C:/Users/VANDANA/AppData/Local/Programs/Python/Python311/relational.py
+False
+True
+False
+True
+False
+True
+True
+False
+True
+False
+True
+False
+True
+False
+True
+False
+True
+False
+True
+<class 'tuple'>
+
+= RESTART: C:/Users/VANDANA/AppData/Local/Programs/Python/Python311/relational.py
+False
+True
+False
+True
+False
+True
+True
+False
+True
+False
+True
+False
+True
+False
+True
+False
+True
+False
+True
+<class 'tuple'>
+False
+
+= RESTART: C:/Users/VANDANA/AppData/Local/Programs/Python/Python311/relational.py
+False
+True
+False
+True
+False
+True
+True
+False
+True
+False
+True
+False
+True
+False
+True
+False
+True
+False
+True
+<class 'tuple'>
+False
+
+= RESTART: C:/Users/VANDANA/AppData/Local/Programs/Python/Python311/relational.py
+False
+True
+False
+True
+False
+True
+True
+False
+True
+False
+True
+False
+True
+False
+True
+False
+True
+False
+True
+<class 'tuple'>
+False
+
+= RESTART: C:/Users/VANDANA/AppData/Local/Programs/Python/Python311/relational.py
+False
+True
+False
+True
+False
+True
+True
+False
+True
+False
+True
+False
+True
+False
+True
+False
+True
+False
+True
+<class 'tuple'>
+False
+
+= RESTART: C:/Users/VANDANA/AppData/Local/Programs/Python/Python311/relational.py
+False
+True
+False
+True
+False
+True
+True
+False
+True
+False
+True
+False
+True
+False
+True
+False
+True
+False
+True
+<class 'tuple'>
+False
+False
+
+= RESTART: C:/Users/VANDANA/AppData/Local/Programs/Python/Python311/relational.py
+False
+True
+False
+True
+False
+True
+True
+False
+True
+False
+True
+False
+True
+False
+True
+False
+True
+False
+True
+<class 'tuple'>
+False
+False
+
+= RESTART: C:/Users/VANDANA/AppData/Local/Programs/Python/Python311/relational.py
+False
+True
+False
+True
+False
+True
+True
+False
+True
+False
+True
+False
+True
+False
+True
+False
+True
+False
+True
+<class 'tuple'>
+False
+False
+
+= RESTART: C:/Users/VANDANA/AppData/Local/Programs/Python/Python311/relational.py
+False
+True
+False
+True
+False
+True
+True
+False
+True
+False
+True
+False
+True
+False
+True
+False
+True
+False
+True
+<class 'tuple'>
+False
+False
+False
+
+= RESTART: C:/Users/VANDANA/AppData/Local/Programs/Python/Python311/relational.py
+False
+True
+False
+True
+False
+True
+True
+False
+True
+False
+True
+False
+True
+False
+True
+False
+True
+False
+True
+<class 'tuple'>
+False
+False
+False
+Traceback (most recent call last):
+  File "C:/Users/VANDANA/AppData/Local/Programs/Python/Python311/relational.py", line 45, in <module>
+    print(D1<D2)
+TypeError: '<' not supported between instances of 'dict' and 'dict'
+
+= RESTART: C:/Users/VANDANA/AppData/Local/Programs/Python/Python311/relational.py
+False
+True
+False
+True
+False
+True
+True
+False
+True
+False
+True
+False
+True
+False
+True
+False
+True
+False
+True
+<class 'tuple'>
+False
+False
+False
+Traceback (most recent call last):
+  File "C:/Users/VANDANA/AppData/Local/Programs/Python/Python311/relational.py", line 45, in <module>
+    print(D1<2)
+TypeError: '<' not supported between instances of 'dict' and 'int'
+
+= RESTART: C:/Users/VANDANA/AppData/Local/Programs/Python/Python311/relational.py
+False
+True
+False
+True
+False
+True
+True
+False
+True
+False
+True
+False
+True
+False
+True
+False
+True
+False
+True
+<class 'tuple'>
+False
+False
+False
+Traceback (most recent call last):
+  File "C:/Users/VANDANA/AppData/Local/Programs/Python/Python311/relational.py", line 45, in <module>
+    print(D1>D2)
+TypeError: '>' not supported between instances of 'dict' and 'dict'
+
+= RESTART: C:/Users/VANDANA/AppData/Local/Programs/Python/Python311/relational.py
+False
+True
+False
+True
+False
+True
+True
+False
+True
+False
+True
+False
+True
+False
+True
+False
+True
+False
+True
+<class 'tuple'>
+False
+False
+False
+False
+
+= RESTART: C:/Users/VANDANA/AppData/Local/Programs/Python/Python311/relational.py
+False
+True
+False
+True
+False
+True
+True
+False
+True
+False
+True
+False
+True
+False
+True
+False
+True
+False
+True
+<class 'tuple'>
+False
+False
+False
+True
+
+= RESTART: C:/Users/VANDANA/AppData/Local/Programs/Python/Python311/relational.py
+False
+True
+False
+True
+False
+True
+True
+False
+True
+False
+True
+False
+True
+False
+True
+False
+True
+False
+True
+<class 'tuple'>
+False
+False
+False
+False
+
+= RESTART: C:/Users/VANDANA/AppData/Local/Programs/Python/Python311/relational.py
+False
+True
+False
+True
+False
+True
+True
+False
+True
+False
+True
+False
+True
+False
+True
+False
+True
+False
+True
+<class 'tuple'>
+False
+False
+False
+Traceback (most recent call last):
+  File "C:/Users/VANDANA/AppData/Local/Programs/Python/Python311/relational.py", line 46, in <module>
+    print(D1<D2)
+TypeError: '<' not supported between instances of 'dict' and 'dict'
+
+= RESTART: C:/Users/VANDANA/AppData/Local/Programs/Python/Python311/relational.py
+False
+True
+False
+True
+False
+True
+True
+False
+True
+False
+True
+False
+True
+False
+True
+False
+True
+False
+True
+<class 'tuple'>
+False
+False
+False
+Traceback (most recent call last):
+  File "C:/Users/VANDANA/AppData/Local/Programs/Python/Python311/relational.py", line 47, in <module>
+    print(D1<D2)
+TypeError: '<' not supported between instances of 'dict' and 'dict'
---
+++ relational.py
@@ -0,0 +1,54 @@
+N1 = 'ABC'
+N2 = 'Aef'
+print(N1>=N2)
+print(N1<N2)
+print(N1>N2)
+print(N1<=N2)
+print(N1==N2)
+print(N1!=N2)
+
+L1 = [10,20,30]
+L2 = [20,'RAM',30,45]
+print(L1<L2)
+L3 = ['RAM',20,30]
+L4 = ['RAJ',45,60]
+print(L3<L4)
+print(L3>L4)
+print(L3<=L4)
+print(L3>=L4)
+print(L1==L2)
+print(L1!=L2)
+
+
+T1 = ('abc',10,20,30,40)
+T2 = ('ABC',25,46,50)
+print(T1 < T2)
+print(T1 > T2)
+print(T1 <= T2)
+print(T1 >= T2)
+print(T1 == T2)
+print(T1 != T2)
+print(type(T1))
+
+S1={-10,20,30,40}
+S2={'STR',30,50,70}
+print(S1>S2)
+S3={10,20}
+S4={10,20}
+print(S1==S2)
+S5 = {10,20,30}
+S6 = {10,40,50}
+print(S5<S6)
+
+D1={'X':10,'Y':30}
+D2={'X':10,'Y':30}
+
+
+print(D1<D2)
+
+
+
+
+
+
+

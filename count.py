2c36f8c5ccfa5884a3619c7f81e6ef14700fa60c
APPIREDDIGARI VANDANA <135859797+VANDANAREDDY73@users.noreply.github.com>
2023-07-17 22:43:52 +0530

    Add files via upload

---
+++ count.py
@@ -0,0 +1,16 @@
+#COUNT METHOD: It returns occurance of substring in given string.
+#Syntax: VariableName.count(arg1)
+s= 'we are developers'
+print(s.count('e'))
+print(s.count('er'))
+print(s.count('rer'))
+print(s.count('d'))
+
+c='misissippi'
+print(c.count('i'))
+print(c.count('s'))
+print(c.count('p'))
+print(c.count('si'))
+
+
+

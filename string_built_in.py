66e4d850a4f38bde4a85cb5329f96a3db3c70726
APPIREDDIGARI VANDANA <135859797+VANDANAREDDY73@users.noreply.github.com>
2023-07-17 21:51:22 +0530

    Add files via upload

---
+++ string_built_in.py
@@ -0,0 +1,13 @@
+s = 'We are DeveloPERS'
+print(s.lower())
+print(s.upper())
+print(s.swapcase())
+print(s.title())
+print(s.capitalize())
+
+c = 'i Am pythoN DeveloPER@123'
+print(c.lower())
+print(c.upper())
+print(c.swapcase())
+print(c.title())
+print(c.capitalize())
---
+++ string_built_in_s.py
@@ -0,0 +1,14 @@
+Python 3.11.4 (tags/v3.11.4:d2340ef, Jun  7 2023, 05:45:37) [MSC v.1934 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+= RESTART: C:/Users/VANDANA/AppData/Local/Programs/Python/Python311/string_built_in.py
+we are developers
+WE ARE DEVELOPERS
+wE ARE dEVELOpers
+We Are Developers
+We are developers
+i am python developer@123
+I AM PYTHON DEVELOPER@123
+I aM PYTHOn dEVELOper@123
+I Am Python Developer@123
+I am python developer@123

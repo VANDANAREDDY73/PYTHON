7f07ac05e7d98f6db6a2e12b0d46d82d3d5abf3a
APPIREDDIGARI VANDANA <135859797+VANDANAREDDY73@users.noreply.github.com>
2023-07-22 19:08:42 +0530

    Add files via upload

---
+++ sort.py
@@ -0,0 +1,33 @@
+S = ['12','15','10','ABC']
+S.sort()
+print(S)
+
+L = ['ABC','abc','2','89','100','-3','2.5']
+L.sort(reverse=True)
+print(L)
+
+M = [[2,4],[5,4],[2,3],[4,20]]
+M.sort()
+print(M)
+
+N = [['a','A'],['cde','ant'],['A','ert']]
+N.sort(reverse=True)
+print(N)
+
+P = [(2,3,4),(1,5,8),(40,70,35),(20,30,40)]
+P.sort()
+print(P)
+
+Q = [('v','s','a'),('s','E'),('A','B','C')]
+Q.sort(reverse=True)
+print(Q)
+
+A = [{1,5,7}, {23,56,87}, {20,40,50}]
+A.sort()
+print(A)
+
+B = {'C':50,'A':70,'G':20}
+B.sort()
+print(B)
+
+
---
+++ sort_s.py
@@ -0,0 +1,15 @@
+Python 3.11.4 (tags/v3.11.4:d2340ef, Jun  7 2023, 05:45:37) [MSC v.1934 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+= RESTART: C:/Users/VANDANA/AppData/Local/Programs/Python/Python311/sort.py
+['10', '12', '15', 'ABC']
+['abc', 'ABC', '89', '2.5', '2', '100', '-3']
+[[2, 3], [2, 4], [4, 20], [5, 4]]
+[['cde', 'ant'], ['a', 'A'], ['A', 'ert']]
+[(1, 5, 8), (2, 3, 4), (20, 30, 40), (40, 70, 35)]
+[('v', 's', 'a'), ('s', 'E'), ('A', 'B', 'C')]
+[{1, 5, 7}, {56, 87, 23}, {40, 50, 20}]
+Traceback (most recent call last):
+  File "C:/Users/VANDANA/AppData/Local/Programs/Python/Python311/sort.py", line 30, in <module>
+    B.sort()
+AttributeError: 'dict' object has no attribute 'sort'

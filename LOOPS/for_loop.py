2ec5d15ca5652be059a45d82f85e365dc2c9ac0a
APPIREDDIGARI VANDANA <135859797+VANDANAREDDY73@users.noreply.github.com>
2023-07-28 20:25:55 +0530

    Add files via upload

---
+++ LOOPS/for_loop.py
@@ -0,0 +1,35 @@
+for x in 'python@123':
+    print(x)
+for y in 'developer':
+    print('hi')
+
+L=[10,20,'abc',(30,40),'d']
+for value in L:
+    print(value)
+for value in L:
+    print('Hello')
+
+T=(10,20,'A','C',70)
+for value in T:
+    print(value)
+for value in T:
+    print('python')
+
+S={10,20,'abc',(20,40)}
+for value in S:
+    print(value)
+for value in S:
+    print('developer')
+
+D={'a':10,'b':20,(10,20):70}
+for key in D:
+    print(key)
+for key in D:
+    print('Hey')
+
+
+
+
+
+
+
---
+++ LOOPS/for_loop_s.py
@@ -0,0 +1,57 @@
+Python 3.11.4 (tags/v3.11.4:d2340ef, Jun  7 2023, 05:45:37) [MSC v.1934 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+= RESTART: C:/Users/VANDANA/AppData/Local/Programs/Python/Python311/for_loop.py
+p
+y
+t
+h
+o
+n
+@
+1
+2
+3
+hi
+hi
+hi
+hi
+hi
+hi
+hi
+hi
+hi
+10
+20
+abc
+(30, 40)
+d
+Hello
+Hello
+Hello
+Hello
+Hello
+10
+20
+A
+C
+70
+python
+python
+python
+python
+python
+(20, 40)
+10
+abc
+20
+developer
+developer
+developer
+developer
+a
+b
+(10, 20)
+Hey
+Hey
+Hey

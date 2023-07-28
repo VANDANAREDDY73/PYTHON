2cabdad1102cc793def472d6089321cb3342e379
APPIREDDIGARI VANDANA <135859797+VANDANAREDDY73@users.noreply.github.com>
2023-07-28 20:43:09 +0530

    Add files via upload

---
+++ LOOPS/range_function.py
@@ -0,0 +1,17 @@
+for x in range(15,24):
+    print(x)
+for x in range(-15,24):
+    print(x)
+for x in range(15,-24,-1):
+    print(x)
+for x in range(-15,-24,-1):
+    print(x)
+for x in range(23,14,-1):
+    print(x)
+for x in range(-23,16):
+    print(x)
+for x in range(23,-16,-1):
+    print(x)
+for x in range(-23,-14):
+    print(x)
+    

7b8abdcc3cc9f4c4a0365d0b430c00471f0a6075
APPIREDDIGARI VANDANA <135859797+VANDANAREDDY73@users.noreply.github.com>
2023-07-29 20:32:50 +0530

    Add files via upload

---
+++ LOOPS/while_loop.py
@@ -0,0 +1,54 @@
+num = 2
+while (num!=10):
+    print(num)
+    num=num+2
+num = 34
+while (num!=22):
+    print(num)
+    num=num-1
+num = -34
+while (num!=24):
+    print(num)
+    num=num+1
+num = 34
+while (num!=-24):
+    print(num)
+    num=num-1
+num = -34
+while (num!=-22):
+    print(num)
+    num=num+1
+num = 100
+while (num!=0):
+    print(num)
+    num=num//2
+num = 1
+while (num!=11):
+    print(num)
+    num=num+1
+num = 10
+while (num!=0):
+    print(num)
+    num=num-1
+num = 23
+while (num!=35):
+    print(num)
+    num=num+1
+num = -23
+while (num!=35):
+    print(num)
+    num=num+1
+num = 23
+while (num!=-35):
+    print(num)
+    num=num-1
+num = -23
+while (num!=-35):
+    print(num)
+    num=num-1
+
+
+
+
+
+    

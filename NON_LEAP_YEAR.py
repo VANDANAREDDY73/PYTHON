a2d1a0cc0c2cb7e323083fb63833d679911c7ef1
APPIREDDIGARI VANDANA <135859797+VANDANAREDDY73@users.noreply.github.com>
2023-09-10 18:29:36 +0530

    Update NON_LEAP_YEAR.py

--- NON_LEAP_YEAR.py
+++ NON_LEAP_YEAR.py
@@ -1,11 +1,27 @@
-year=2021
-if (year%100==0):
-    if (year%400==0):
+#write a program to print given year is leap_year or not
+#CONDITIONS:
+#1-Century leap year is divisible with 400
+#2-Non-Century leap year is divisible with 4 and not divisible with 100
+
+#Method -1
+year=2021  #Enter the year
+if (year%100==0):   
+    if (year%400==0):# Checking condition for century leap year
         print('LEAP YEAR')
     else:
         print('NON-LEAP YEAR')
 else:
-    if (year%4==0):
+    if (year%4==0): #Checking condition for Non-century leap year
         print('LEAP YEAR')
     else:
         print('NON-LEAP YEAR')
+
+#Method -2
+year = 2024
+if (year%100 == 0) and (year%400 == 0):
+    print('Leap year')
+elif (year%4 == 0):
+    print('Leap year')
+else:
+    print('Not leap year')
+

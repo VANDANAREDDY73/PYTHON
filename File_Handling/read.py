ca4cb6f6e5829415c5010f3196558932c29e521e
APPIREDDIGARI VANDANA <135859797+VANDANAREDDY73@users.noreply.github.com>
2023-11-26 19:55:50 +0530

    Update read.py

--- File_Handling/read.py
+++ File_Handling/read.py
@@ -8,3 +8,17 @@ fileob.seek(0)
 print(fileob.readlines())
 print(fileob.tell())
 fileob.close()
+
+solution:
+0
+Python full stack developer
+,Sql is a data base
+,Web technologies is used for designing web pages10
+abc
+hello
+hi
+117
+True
+
+['Python full stack developer\n', ',Sql is a data base\n', ',Web technologies is used for designing web pages10\n', 'abc\n', 'hello\n', 'hi']
+117
